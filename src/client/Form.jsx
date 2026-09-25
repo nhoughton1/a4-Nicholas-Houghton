b521ae737c4a987d4762007de0268c115641944e
nhoughton1 <MACARONIHOUGHTON@GMAIL.COM>
2026-09-25 17:35:46 -0400

    Comments/consistency

--- src/client/Form.jsx
+++ src/client/Form.jsx
@@ -7,9 +7,9 @@ function Form(props) {
     const [total, setTotal] = useState(0)
     const [modifySelector, setModifySelector] = useState('')
 //submit show
-    const submit = function(event){
+    const submit = function(e){
 
-        event.preventDefault()
+        e.preventDefault()
         const json = {
             show: show,
             watched: Number(watched),
@@ -30,26 +30,26 @@ function Form(props) {
     return (
         //displays the form
         <form onSubmit={submit}>
+            {/*showname*/}
             <label className="form-group" htmlFor="showname">Show</label>
-            <input className="form-group" type='text' id='showname' value={show} placeholder = "Name Of Show Here"
-            onChange={(event) => setShow(event.target.value)}/>
-
+            <input className="form-group" type='text' id='showname' value={show} placeholder = "Name of Show Here"
+            onChange={(e) => setShow(e.target.value)}/>
+            {/*epsidoes watched*/}
             <label className="form-group" htmlFor="episodeswatched">Episodes Watched</label>
             <input className="form-group" type='number' id='episodeswatched' value={watched}
-            onChange={event => setWatched(event.target.value)}/>
+            onChange={e => setWatched(e.target.value)}/>
 
-
+            {/*total episodes*/}
             <label className="form-group" htmlFor="episodecount">Total Episodes</label>
             <input className="form-group" type='number' id='episodecount' value={total}
-            onChange={event => setTotal(event.target.value)}/>
+            onChange={e => setTotal(e.target.value)}/>
             <div className="form-buttons">
                 <button id="submitbutton" type="submit">Submit</button>
                 <span>OR</span>
                 <button type="button" id="modifybutton" onClick={modifyShow}>Modify</button>
                 <div className="modify-show">
                     <label htmlFor="index">Show to Modify</label>
-
-                    <select id='index' value={modifySelector} onChange={(event) => setModifySelector(event.target.value)}>
+                    <select id='index' value={modifySelector} onChange={(e) => setModifySelector(e.target.value)}>
                         <option value = "">
                             Select
                         </option>
